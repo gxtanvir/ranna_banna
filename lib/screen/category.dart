6abc64614a335c7a501d840996e02b9428068301
gxtanvir <gxtanvir@gmail.com>
2023-08-26 11:48:33 +0600

    Animation

--- lib/screen/category.dart
+++ lib/screen/category.dart
@@ -5,31 +5,63 @@ import 'package:ranna_banna/model/meal.dart';
 import 'package:ranna_banna/screen/meals.dart';
 import 'package:ranna_banna/widget/category_grid_item.dart';
 
-class CategoryScreen extends StatelessWidget {
+class CategoryScreen extends StatefulWidget {
   const CategoryScreen({
     super.key,
     required this.availableMeal,
   });
   final List<Meal> availableMeal;
 
+  @override
+  State<CategoryScreen> createState() => _CategoryScreenState();
+}
+
+class _CategoryScreenState extends State<CategoryScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+      lowerBound: 0,
+      upperBound: 1,
+    );
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
   // For Filtering Meal And Navigate to Meals Screen
   void _selectCategory(BuildContext context, Category category) {
-    final filterdMeals = availableMeal
+    final filterdMeals = widget.availableMeal
         .where((meal) => meal.category.contains(category.id))
         .toList();
     Navigator.of(context).push(
       MaterialPageRoute(
         builder: (ctx) => MealScreen(
-            title: category.title,
-            meals: filterdMeals,),
+          title: category.title,
+          meals: filterdMeals,
+        ),
       ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: GridView(
+    return AnimatedBuilder(
+      animation: _animationController,
+      builder: (context, child) => Padding(
+        padding: EdgeInsets.only(top: 100 - _animationController.value * 100),
+        child: child,
+      ),
+      child: GridView(
         padding: const EdgeInsets.all(24),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
